954ae538c8ef4d4e422b2df443cb98f08c35ffff
Oceane Patiny <oceane@patiny.com>
2022-03-10 15:53:06 +0100

    feat: create new folder for reusable functions

---
+++ custom-functions/auto_subplots.m
@@ -0,0 +1,16 @@
+%% Plot N different discrete signals in subplots
+function auto_subplots(signals, height, width) 
+index = 1;
+figure();
+for i=1:2
+    for j=1:3
+        subplot(height,width,index);
+        nn = 0:length(signals{index})-1;
+        plot(nn,signals{index},'.');
+        xlabel('n [-]');
+        title(strcat('x', string(index), '[n]'));
+        grid on;
+        index = index+1;
+    end
+end
+end
