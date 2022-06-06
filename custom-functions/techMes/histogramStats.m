51271e0aad872de5cd6f0eb460fb36fd3ee2f430
Oceane Patiny <oceane@patiny.com>
2022-06-06 13:51:47 +0200

    fix(techmes): fix histogramStats

--- custom-functions/techMes/histogramStats.m
+++ custom-functions/techMes/histogramStats.m
@@ -1,22 +1,21 @@
-function [average,std,median, cumulatedFrequencies] = histogramStats(histData, edges)
+function [average, std, median, cumulatedFrequencies] = histogramStats(histData, binsMiddle)
 % histogramStats: Find average and standard deviation of the data of a
 %                 histogram
 %   - histData: nb of occurences in each class
-%   - edges: edges of the bins of the histogram
+%   - binsMiddle: edges of the bins of the histogram
 % returns: 
 %   - average: average of the histogram data
 %   - std: standard deviation of the histogram
 %   - median: median of the data (linearily interpolated)
 %   - cumulatedFrequencies: the cumulated histogram of the frequencies
 %                           (between 0 and 1)
-classWidth = edges(2)-edges(1);
 nbValues = sum(histData);
 nbClasses = length(histData);
 
-normalisedHistogram = histData/nbValues;
+nbValues
+normalisedHistogram = histData/nbValues; % probability for each value
 
 %% computing average
-binsMiddle = edges(2:end) - classWidth/2;
 average = sum(normalisedHistogram .* binsMiddle);
 
 %% computing standard deviation
@@ -37,8 +36,8 @@ index = find(cumulatedFrequencies>=0.5, 1);
 
 ci = binsMiddle;
 fi = cumulatedFrequencies;
+
 % linear interpolation between the two values around a frequency of 50%
-ci(index-1)
 median = (ci(index-1)-ci(index))/(fi(index-1)-fi(index))*(0.5-fi(index-1)) + ci(index-1);
 end
 
