ceec79b4469137ab10fbe569edf0c902cc94f64f
Oceane Patiny <oceane@patiny.com>
2022-06-06 13:52:07 +0200

    fix(signsys): autosubplot has probably a bug

--- custom-functions/signaux-systemes/auto_subplots.m
+++ custom-functions/signaux-systemes/auto_subplots.m
@@ -2,8 +2,8 @@
 function auto_subplots(signals, height, width) 
 index = 1;
 figure();
-for i=1:2
-    for j=1:3
+for i=1:height
+    for j=1:width
         subplot(height,width,index);
         nn = 0:length(signals{index})-1;
         plot(nn,signals{index},'.');
