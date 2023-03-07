9e4c994cfad31085ba0c186b78f45521e2d651b8
Oceane Patiny <oceane@patiny.com>
2023-03-07 15:04:29 +0100

    regul: finish ex1.1

--- regul/exercices/ex1_1_variables.m
+++ regul/exercices/ex1_1_variables.m
@@ -1,8 +1,11 @@
 %% Exercice 1.1
 clear; clc; close all;
 
+%% Simulation parameters
+calculationPrecision = 0.1; % [s]
+
 %% Variables
-simTime = 30; % [s]
+simTime = 60; % [s]
 
 % gains
 Kp = 20; % [V/V]
@@ -10,13 +13,16 @@ Ka1 = 1250/3; % [N/V]
 Kmv = 0.18; % speed sensor [V/m/s]
 
 % other
-vc = 80;
+vc = 120; % [km/h]
 vc = vc/3.6/Kp; % [m/s]
 m = 1500; % vehicle mass [kg]
 
 % adding more constraints
 % coming on a slope
-tSlope = 15; % [s]
+tSlope = 30; % [s]
 g = 9.81; % [m/s/s]
 alpha = deg2rad(10); % [rad]
+
+% adding a delay
+delay = pi/3; % [s]
 
