8d9f887cbb396b68b5ecde01a49baab771512375
Oceane Patiny <oceane@patiny.com>
2023-11-16 16:15:44 +0100

    sem: labo 2 suite

--- sysElMec/labo2/plot_data.m
+++ sysElMec/labo2/plot_data.m
@@ -17,14 +17,32 @@ p_Wmag = polyfit(alpha, WmagPrime, 11);
 WmagPrime_model = polyval(p_Wmag, alpha);
 
 
-p_psi_ba = polyfit(alpha, psi_ba, 4);
+p_psi_ba = polyfit(alpha, psi_ba, 11);
 psi_ba_model = polyval(p_psi_ba, alpha);
 
-p_psi_bb = polyfit(alpha, psi_bb, 4);
+p_psi_bb = polyfit(alpha, psi_bb, 11);
 psi_bb_model = polyval(p_psi_bb, alpha);
 
 %% compute torques
-coupleReluctant = polyder(p_Wmag);
+p_W_der = polyder(p_Wmag);
+coupleReluctant = polyval(p_W_der, alpha);
+
+p_psi_ba_der = polyder(p_psi_ba);
+coupleMutuel = polyval(p_psi_ba_der, alpha)*Ib;
+
+p_psi_bb_der = polyder(p_psi_bb);
+couplePropre = polyval(p_psi_bb_der, alpha)*Ib;
+
+
+% couples en alpha = 0
+startAngle = 5;
+endAngle = 20;
+
+Tem_aa = mean(coupleReluctant(startAngle:endAngle))*1e3
+Tem_ba = mean(coupleMutuel(startAngle:endAngle))*1e3
+Tem_bb = mean(couplePropre(startAngle:endAngle))*1e3
+
+Tem = Tem_aa + Tem_ba + Tem_bb
 
 %% plot results
 figure();
@@ -43,4 +61,14 @@ hold off;
 legend('\Psi_{bb}', '\Psi_{ba}', 'Location', 'southwest');
 grid on;
 xlabel('\alpha [°]');
-ylabel('Flux propres et mutuels [wb]');
+ylabel('Flux totalisés propres et mutuels [wb]');
+
+%% plot torques
+figure();
+plot(alpha, coupleReluctant, 'b-'); hold on;
+plot(alpha, coupleMutuel, 'r-');
+plot(alpha, couplePropre, 'g-');
+grid on;
+legend('Couple réluctant', 'Couple mutuel', 'Couple propre', 'Location', 'southwest');
+xlabel('\alpha [°]');
+ylabel('Couple [Nm]');
