96fdf6f998f3b82bf700eaabdbccdf9685a79981
Oceane Patiny <oceane@patiny.com>
2024-04-28 17:35:58 +0200

    sim: resotre hw06 ex3

--- simulationMatlab/homeworks/hw06/hw06_ex3.m
+++ simulationMatlab/homeworks/hw06/hw06_ex3.m
@@ -7,7 +7,29 @@ C1 = [0,0,0];
 C2 = [1,0,0];
 C3 = [0,1,0]
 
-
-
 %% Define quations
-@(R,C)(x-C(1))^2+(y-C(2))^2+(z-C(3))^2==R^2
+syms x y z;
+sphereEq = @(R,C)(x-C(1))^2+(y-C(2))^2+(z-C(3))^2==R^2;
+
+eq1 = sphereEq(R, C1);
+eq2 = sphereEq(R, C2);
+eq3 = sphereEq(R, C3);
+
+%% Solve
+
+[Px, Py, Pz] = vpasolve(eq1, eq2, eq3, x, y, z, [0,0,0])
+
+%% Plot
+
+[X,Y,Z] = sphere;
+
+X2 = X+1;
+Y3 = Y+1;
+
+figure();
+hold on;
+mesh(X,Y,Z, 'EdgeColor', 'yellow', 'FaceAlpha', 0.5);
+mesh(X2,Y,Z, 'EdgeColor', 'green', 'FaceAlpha', 0.5);
+mesh(X,Y3,Z, 'EdgeColor', 'blue', 'FaceAlpha', 0.5);
+scatter3(Px, Py, Pz, 100, 'r.');
+hold off;
