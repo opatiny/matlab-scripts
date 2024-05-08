db19b2baa1d028d95e80098dd27418d6468049ac
Oceane Patiny <oceane@patiny.com>
2024-05-08 09:16:58 +0200

    sim: finish hw07
    There is still a bug with the display of imagesc, colors not right.

--- simulationMatlab/homeworks/magneticFields/magField5.m
+++ simulationMatlab/homeworks/magneticFields/magField5.m
@@ -5,7 +5,7 @@ clear; clc; clf;
 % should give → Hz = 0.0768 (A/m), Hr = 0.091 (A/m)
 [Hz,Hr] = Hloop(1,1,1)
 
-%% Plot magnetic field
+%% Compute magnetic field with mesh grid
 % plot grid
 N = 100;
 
@@ -17,9 +17,26 @@ z = linspace(-0.02,0.02,N);
 Rloop = 0.02; % m
 [Hz,Hr]=Hloop(Rloop,R,Z);
 
-H = (Hr.^2 + Hz.^2).^0.5;
+getH = @(Hr,Hz) (Hr.^2 + Hz.^2).^0.5;
 
-% plot vectorfield
+H = getH(Hr,Hz);
+
+% Superimpose field of two loops
+% calculate vector field on grid
+Rloop = 0.02; % m
+[Hz1,Hr1]=Hloop(Rloop,R,Z, [0,-0.01]);
+[Hz2,Hr2]=Hloop(Rloop,R,Z, [0,0.01]);
+
+Hr = Hr1 + Hr2;
+Hz = Hz1 + Hz2;
+
+H = getH(Hr,Hz);
+
+%% Magnetic field on axis
+Hz_r0 = Hz(:,1);
+
+%% Plot vectorfield
+subplot(1,3,1:2);
 imagesc(r,z,H);
 colormap(jet);
 colorbar;
@@ -29,17 +46,31 @@ set(p,'LineWidth', 1)
 set(p,'Color','w');
 hold off;
 axis equal, axis tight
-xlabel('r'),ylabel('z')
+xlabel('r [m]'),ylabel('z [m]');
+
+subplot(1,3,3);
+plot(z,Hz_r0,'-');
+xlabel('z [m]');
+ylabel('H_z [A/m]');
+grid on;    
 
 %% Function Hloop
-function [Hz, Hr] = Hloop(R,r,z)
+function [Hz, Hr] = Hloop(R,r,z, C)
 % Magnetic field of a current loop at point P
 %   R: radius of the loop [m]
 %   r: radius of P in polar coordinate 
-%   z: z of P in polar coordinate 
+%   z: z of P in polar coordinate
+%   C (opt): center of the loop as an [r,z] point, default [0,0]
 % returns:
 %   Hz: z component of mag field [A/m]
 %   Hr: radial component of the mag field [A/m]
+
+if ~exist('C','var')
+    C = [0,0];
+end
+
+r = r-C(1);
+z = z-C(2);
 
 I = 1; % A
 mu0 = 4*pi*1e-7; % N/A^2
