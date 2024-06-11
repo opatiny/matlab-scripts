8045ecf8b0eb5ae20f2fa3e7ffb887aecbb52083
Oceane Patiny <oceane@patiny.com>
2024-06-11 17:00:00 +0200

    sim: ball exercice

--- simulationMatlab/homeworks/hw11/hw11_ex2.m
+++ simulationMatlab/homeworks/hw11/hw11_ex2.m
@@ -5,14 +5,16 @@ clc; clear; clf;
 %% define grid
 dimension = 1000;
 holeSide = 3;
-n = 12;
+n = 5;
 radius = 30;
+
+maxValue = 1;
 
 
 grid = zeros(dimension, dimension);
 
 increment = 2*pi/n;
-angleOffset = pi/2
+angleOffset = pi/2;
 
 for angle = 0+angleOffset:increment:2*pi-increment+angleOffset
     centerRow = round(dimension/2 + radius*sin(angle));
@@ -20,7 +22,7 @@ for angle = 0+angleOffset:increment:2*pi-increment+angleOffset
     
     r = (holeSide-1)/2;
 
-    grid(centerRow-r:centerRow+r, centerColumn-r:centerColumn+r) = 1;
+    grid(centerRow-r:centerRow+r, centerColumn-r:centerColumn+r) = maxValue;
 end
 
 
@@ -34,7 +36,7 @@ ylim([400,600]);
 
 % compute diffraction image with fft2
 subplot(122);
-imagesc(abs(fft2(grid)));axis equal
+imagesc(abs(fft2(grid)));axis equal;
 colormap("gray");
 xlim([0,1000]);
 ylim([0,1000]);
