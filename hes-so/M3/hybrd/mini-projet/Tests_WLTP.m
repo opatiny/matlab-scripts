7163f133414d6e74aad70b35f8aab063c0668c1b
Oceane Patiny <oceane@patiny.com>
2025-12-15 13:55:04 +0100

    hybrd: final tests of strategy

--- hes-so/M3/hybrd/mini-projet/Tests_WLTP.m
+++ hes-so/M3/hybrd/mini-projet/Tests_WLTP.m
@@ -6,7 +6,6 @@
 %   Comment load of cycles: 
 %   Comment all of what is related to simulation parametersy, load WLTP.mat % test profile
 
-
 clear; close all; clc
 
 %% Test in full electic mode
@@ -17,7 +16,7 @@ load WLTP.mat % test profile
 BATp.initSoC=100/100;        % Battery initial SoC (per unit)
 BATp.SOC_max = 90; % (%)
 BATp.SOC_min = 20; % (%)
-SOC_min_stop = 2; % (%)
+SOC_min_stop = BATp.SOC_min; % (%)
 
 Test_Elec = 1; % fully electric mode, thermal motor off
 
@@ -37,11 +36,15 @@ disp(Perf);
 %% Test in depletion mode
 disp('Test depletion mode')
 
+Perf.Time = 7.6657e+03;
+Perf.Distance = 95.7709;
+Perf.kWh_batt= 19.7964;
+
 load WLTP.mat % test profile
 BATp.initSoC = 5/100;        % Battery initial SoC (per unit)
 Test_Elec = 0;
 BATp.SOC_min = 20; % (%)
-SOC_min_stop = BATp.SOC_min; % (%)
+SOC_min_stop = 2; % (%)
 
 SIM.t_min = 0;                    % Simulation beginning
 SIM.t_simul = max(CYCL.time)+10;  % Simulation end
@@ -50,11 +53,12 @@ sim('HYV_EMR_IBC_Macro.slx');
 
 
 Perf.Time = [Perf.Time Time_Final];
-Perf.Distance = [Perf.Distance Distance_Final];
+Perf.Distance = [Perf.Distance Distance_Final]; % in km
 Perf.kWh_batt = [Perf.kWh_batt kWh_Final];
-Perf.Conso_elec = [100*Perf.kWh_batt(1)/Perf.Distance(1) 100*sum(Perf.kWh_batt)/sum(Perf.Distance)];
+Perf.Conso_elec = [100*Perf.kWh_batt(1)/Perf.Distance(1) 100*sum(Perf.kWh_batt)/sum(Perf.Distance)]; % kWh/100km
 Perf_Fuel_liter = [0 Liter_Final];
 Perf.Fuel_Conso = [ 0 100*Liter_Final/sum(Perf.Distance) 100*Liter_Final/sum(Perf.Distance)*(1-Perf.Time(2)/sum(Perf.Time))];
+% 2: , 3: weighted by fraction of time spent in depletion mode
 
 disp(Perf)
 
